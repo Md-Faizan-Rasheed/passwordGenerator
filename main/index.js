179879efd70ac1ab4cdb4e3e5f89d0b7c917b8b5
Md-Faizan-Rasheed <s1391792@gmail.com>
2023-10-17 07:36:48 +0530

    after fetching elements from html to js

---
+++ main/index.js
@@ -0,0 +1,29 @@
+const inputSlider=doucment.querySelector("[data-lengthSlider ]");
+
+const lengthDisplay=document.querySelector("[data-lengthNumber]");
+
+const passwordDisplay=doucment.querySelector("[data-passwordDisplay]");
+
+const copybtn=document.querySelector("[data-copy]");
+
+const copyMsg=document.querySelector("[data-copuMsg]");
+
+const uppercaseCheck=doucment.querySelector("#uppercase");
+
+const lowercaseCheck=doucment.querySelector("#lowercase");
+
+const numbersCheck=document.querySelector("#numbers");
+
+const symbolsCheck=document.querySelector("#symbols");
+
+const indicator=doucment.querySelector("[data-indicator]");
+
+const generateBtn=document.querySelector(".generateBtn");
+
+const allCheckBox=document.querySelectorAll("input[type=checkbox]");
+
+let password=" ";
+
+let passwordLength=10;
+
+let checkCount=1;
