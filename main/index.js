b118b6c38ddd4eef276b19df66b7d84052e05a1d
Md-Faizan-Rasheed <s1391792@gmail.com>
2023-10-18 08:31:49 +0530

    Just Adding some js function

--- main/index.js
+++ main/index.js
@@ -1,29 +1,90 @@
-const inputSlider=doucment.querySelector("[data-lengthSlider ]");
+const inputSlider=document.querySelector("[data-lengthSlider ]");
 
 const lengthDisplay=document.querySelector("[data-lengthNumber]");
 
-const passwordDisplay=doucment.querySelector("[data-passwordDisplay]");
+const passwordDisplay=document.querySelector("[data-passwordDisplay]");
 
 const copybtn=document.querySelector("[data-copy]");
 
-const copyMsg=document.querySelector("[data-copuMsg]");
+const copyMsg=document.querySelector("[data-copyMsg]");
 
-const uppercaseCheck=doucment.querySelector("#uppercase");
+const uppercaseCheck=document.querySelector("#uppercase");
 
-const lowercaseCheck=doucment.querySelector("#lowercase");
+const lowercaseCheck=document.querySelector("#lowercase");
 
-const numbersCheck=document.querySelector("#numbers");
+const numbersCheck=document.querySelector("#Numbers");
 
 const symbolsCheck=document.querySelector("#symbols");
 
-const indicator=doucment.querySelector("[data-indicator]");
+const indicator=document.querySelector("[data-indicator]");
 
-const generateBtn=document.querySelector(".generateBtn");
+const generateBtn=document.querySelector(".generateButton");
 
 const allCheckBox=document.querySelectorAll("input[type=checkbox]");
 
+const symbol='~`!@#$%^&*()_-+={[}]:;"><.,?/';
 let password=" ";
 
 let passwordLength=10;
 
 let checkCount=1;
+handleSlider();
+// set strength circle color to grey
+
+// set passwordLength
+function handleSlider(){
+  inputSlider.value=passwordLength;
+  lengthDisplay.innerText=passwordLength;
+}
+
+// set Indicator
+function setIndicator(color){
+    indicator.style.backgroundColor=color;
+}
+
+function getRndInteger(min,max){
+  return  Math.floor( Math. random()*(max-min))+min;
+}
+
+
+// Generate Random Number
+function generateRandomNumber(){
+  return getRndInteger(0,9);
+}
+
+function generateLowerCase(){
+         return String.fromCharCode(getRndInterger(97,123));
+}
+
+function generateUpperCase(){
+  return String.fromCharCode(getRndInterger(65,91));
+}
+
+function generateSymbol(){
+  const random=getRndInteger(0,symbols.legth);
+  return symbols.charAt(randNum);
+}
+
+function calStrength(){
+  let hasUpper=false;
+  let hasLower=false;
+  let hasNum=false;
+  let hasSym=false;
+
+  if(uppercaseCheck.checked) hasUpper=true;
+  if(lowercaseCheck.checked) hasLower=true;
+  if(numbersChecked) hasNum=true;
+  if(symbolsCheck.Checked) hasSym=true;
+
+  if(hasUpper && hasLower (hasNum || hasSym ) && passwordLength>=8){
+    setIndicator("#0f0");
+  }else if(
+    (hasLower || hasUpper)&&
+    (hasNum || hasSym)&&
+    passwordLength>=6
+  ){
+    setIndicator("#ff0");
+  }else {
+    setIndicator("#f00");
+  }
+}
