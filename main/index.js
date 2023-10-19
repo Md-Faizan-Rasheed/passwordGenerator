94fdd9a0fd335ed8a26843be294536f0d1bfde50
Md-Faizan-Rasheed <s1391792@gmail.com>
2023-10-20 01:16:12 +0530

    Adding all js code

--- main/index.js
+++ main/index.js
@@ -25,9 +25,10 @@ const allCheckBox=document.querySelectorAll("input[type=checkbox]");
 const symbol='~`!@#$%^&*()_-+={[}]:;"><.,?/';
 let password=" ";
 
+// initially
 let passwordLength=10;
 
-let checkCount=1;
+let checkCount=0;
 handleSlider();
 // set strength circle color to grey
 
@@ -36,12 +37,16 @@ function handleSlider(){
   inputSlider.value=passwordLength;
   lengthDisplay.innerText=passwordLength;
 }
+// set strength circle color to gray
+
+
 
 // set Indicator
 function setIndicator(color){
     indicator.style.backgroundColor=color;
-}
+    // shadow
 
+}
 function getRndInteger(min,max){
   return  Math.floor( Math. random()*(max-min))+min;
 }
@@ -52,16 +57,16 @@ function generateRandomNumber(){
   return getRndInteger(0,9);
 }
 
-function generateLowerCase(){
-         return String.fromCharCode(getRndInterger(97,123));
+function generateLowerCase(){                       //a,z
+         return String.fromCharCode(getRndInteger(97,123));
 }
 
-function generateUpperCase(){
-  return String.fromCharCode(getRndInterger(65,91));
+function generateUpperCase(){              //A,Z
+  return String.fromCharCode(getRndInteger(65,91));
 }
 
 function generateSymbol(){
-  const random=getRndInteger(0,symbols.legth);
+  const random=getRndInteger(0,symbols.length);
   return symbols.charAt(randNum);
 }
 
@@ -88,3 +93,139 @@ function calStrength(){
     setIndicator("#f00");
   }
 }
+
+ async function copyContent (){
+  try{
+    await navigator.clipboard.writeText(passwordDisplay.value);
+  copyMsg.innerText="copied";
+  }
+  catch(e){
+    copyMsg.innerText="failed"
+  }
+
+  //t0 make copy wala message visible
+  copyMsg.classList.add("active");
+
+  setTimeout( ()=>{
+    copyMsg.classList.remove("active");
+  },2000);
+ }
+
+function shufflePassword(shufflePassword){
+  // Fisher Yates Method
+for(let i=array.length-1;i>0;i--){
+  const j=Math.floor(Math.random()*(i+1));
+  const temp=array[i];
+  arry[i]=array[j];
+  array[j]=temp;
+}
+let str=" ";
+array.forEach((el)=>(str+=el));
+return str;
+
+}
+
+ function handleCheckBoxChange(){
+  checkCount=0;
+  allCheckBox.forEach((checkbox)=>{
+    if(checkbox.checked){
+      checkCount++;
+    }
+  });
+
+  // special condition
+  if(passwordLength<checkCount){
+    passwordLength=checkCount;
+    handleSlider();
+  }
+ }
+ allCheckBox.forEach((checkbox)=>{
+  checkbox.addEventListener('change',handleCheckBoxChange);
+ })
+
+
+
+ inputSlider.addEventListener('input',(e)=>{
+  passwordLength=e.target.value;
+  handleSlider();
+ })
+
+ copybtn.addEventListener('click',()=>{
+  if(passwordDisplay.value)
+  copyContent();
+  
+ }) 
+
+ generateBtn.addEventListener('click',()=>{
+  // none of the checkbox are Selected
+  if(checkCount==0) return;
+
+  if(passwordLength<checkCount){
+    passwordLength=checkCount;
+    handleSlider();
+  }
+
+  // let's start the journey to find new password
+console.log("Starting the journey");
+// remove old password
+password =" ";
+
+//let's put the stuff mentioned by checkboxes
+// if(uppercaseCheck.checked){
+//   password+=generateUpperCase();
+// }
+
+// if(lowercaseCheck.checked){
+//   password+=generateLowerCase();
+// }
+
+// if(numbersCheck.checked){
+//   password+=generateRandomNumber();
+// }
+
+// if(symbolsCheck.checked){
+//   password+=generateSymbol();
+// }
+
+let funcArr= [];
+
+if(uppercaseCheck.checked){
+  funcArr.push(generateUpperCase);
+}
+if(lowercaseCheck.checked){
+  funcArr.push(generateLowerCase);
+}
+if(numbersCheck.checked){
+  funcArr.push(generateRandomNumber);
+}
+if(symbolsCheck.checked){
+  funcArr.push(generateSymbol);
+}
+
+//compulsary Addition
+
+for(let i=0;i<funcArr.length;i++){
+  password+=funcArr[i]();
+}
+
+console.log("Compulsory addition done");
+
+//remaining addition
+for(let i=0;i<passwordLength-funcArr.length;i++ ){
+  let randIndex=getRndInteger(0,funcArr.length);
+  password+=funcArr[randIndex]();
+}
+console.log("Remaining addition done");
+// shuffle the password
+password=shufflePassword(Array.from(password));
+
+// show in UI
+passwordDisplay.value=password;
+console.log("UI addition done");
+// claculate strength
+calcStrength();
+
+
+
+ });
+
